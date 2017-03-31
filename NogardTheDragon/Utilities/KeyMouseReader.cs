4c9eef0c2dfc677489dcdf1f08ccecd10801cf61
Lily <lily.hammerskog@gmail.com>
2017-03-31 11:41:07 +0200

    KeyMouseReader

---
+++ NogardTheDragon/Utilities/KeyMouseReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+static class KeyMouseReader
+{
+    public static KeyboardState keyState, oldKeyState = Keyboard.GetState();
+    public static MouseState mouseState, oldMouseState = Mouse.GetState();
+    public static Vector2 mousePosition = Vector2.Zero;
+    public static bool KeyPressed(Keys key)
+    {
+        return keyState.IsKeyDown(key) && oldKeyState.IsKeyUp(key);
+    }
+    public static bool KeyDown(Keys key)
+    {
+        return keyState.IsKeyDown(key);
+    }
+    public static bool LeftClick()
+    {
+        return mouseState.LeftButton == ButtonState.Pressed && oldMouseState.LeftButton == ButtonState.Released;
+    }
+    public static bool RightClick()
+    {
+        return mouseState.RightButton == ButtonState.Pressed && oldMouseState.RightButton == ButtonState.Released;
+    }
+    public static bool MiddleClick()
+    {
+        return mouseState.MiddleButton == ButtonState.Pressed && oldMouseState.MiddleButton == ButtonState.Released;
+    }
+    public static bool ScrollUp()
+    {
+        return mouseState.ScrollWheelValue > oldMouseState.ScrollWheelValue;
+    }
+    public static bool ScrollDown()
+    {
+        return mouseState.ScrollWheelValue < oldMouseState.ScrollWheelValue;
+    }
+
+    //Should be called at beginning of Update in Game
+    public static void Update()
+    {
+        oldKeyState = keyState;
+        keyState = Keyboard.GetState();
+        oldMouseState = mouseState;
+        mouseState = Mouse.GetState();
+        mousePosition = new Vector2(mouseState.X, mouseState.Y);
+    }
+}
