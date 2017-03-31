5ac8308d1bf83eed662a1e600ba9d3f288760752
Lily <lily.hammerskog@gmail.com>
2017-03-31 12:20:53 +0200

    Adjustments to the base classes

--- NogardTheDragon/Abilities/BaseAbility.cs
+++ NogardTheDragon/Abilities/BaseAbility.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NogardTheDragon.Objects;
+﻿using NogardTheDragon.Objects;
 
 namespace NogardTheDragon.Abilities
 {
     internal abstract class BaseAbility
     {
+        protected MovingObject Owner;
+
         protected BaseAbility(MovingObject owner)
         {
             Owner = owner;
         }
 
-        protected MovingObject Owner;
-        
         public abstract void TriggerAbility();
     }
 }
