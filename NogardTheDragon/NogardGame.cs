3d7f55bcb640b6706047dfd3a5a08816845267e3
ObettyB <bettybrandstrom92@hotmail.se>
2017-04-04 19:58:54 +0200

    TextureManager

--- NogardTheDragon/NogardGame.cs
+++ NogardTheDragon/NogardGame.cs
@@ -32,14 +32,6 @@ namespace NogardTheDragon
             Content.RootDirectory = "Content";
         }
 
-        // Here I'm just making the textures public, but making the setters private. You can only set the textures from here, but read from everywhere.
-        public static Texture2D PlatformTexture { get; private set; }
-
-        public static Texture2D PlayerSheet { get; private set; }
-
-        public static Texture2D IndicatorLine { get; private set; }
-        public static Texture2D Goal { get; private set; }
-
         public static SpriteFont Font { get; private set; }
 
         protected override void Initialize()
@@ -51,10 +43,7 @@ namespace NogardTheDragon
         {
             SpriteBatch = new SpriteBatch(GraphicsDevice);
 
-            PlatformTexture = Content.Load<Texture2D>(@"plattform");
-            PlayerSheet = Content.Load<Texture2D>(@"playersquare");
-            IndicatorLine = Content.Load<Texture2D>(@"indicatorline");
-            Goal = Content.Load<Texture2D>(@"goal");
+            TextureManager.LoadTextures(Content);
             Font = Content.Load<SpriteFont>(@"font1");
 
             GamePlayManager = new GamePlayManager();
--- NogardTheDragon/Objects/Platforms/MovingPlatform.cs
+++ NogardTheDragon/Objects/Platforms/MovingPlatform.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NogardTheDragon.Objects.Platforms
 {
-    class BouncePlatform : BaseSpecialPlatform
+    class MovingPlatform : BaseSpecialPlatform
     {
     }
 }
