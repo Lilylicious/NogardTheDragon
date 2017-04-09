4ebe942dc225883e3be5d2c4459f6dc5c255e54a
Lowe <lovisakjorning@gmail.com>
2017-04-09 20:05:35 +0200

    Ny menumanager i egen klass
    idek, hej

---
+++ NogardTheDragon/Managers/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace NogardTheDragon.Managers
+{
+    class MenuManager : BaseManager
+    {
+
+
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
