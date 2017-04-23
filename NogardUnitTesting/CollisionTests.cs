d3c10ef0768cce0312e96f658351cbcfac28331c
Lily <lily.hammerskog@gmail.com>
2017-04-23 16:39:40 +0200

    Basics for tests

---
+++ NogardUnitTesting/CollisionTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NogardUnitTesting
+{
+    [TestClass]
+    public class CollisionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
