9536324a4f6d60c2c53d5ca06c0287d806f27f39
ObettyB <bettybrandstrom92@hotmail.se>
2017-04-24 18:35:21 +0200

    Made MainMenu and a "ButtonManager"

---
+++ NogardTheDragon/Managers/ButtonManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using NogardTheDragon.Utilities;
+
+namespace NogardTheDragon.Managers
+{
+    public class ButtonManager : BaseManager
+    {
+        public StandardButton PlayButton;
+        public StandardButton ScoreButton;
+        public StandardButton ExitButton;
+        public StandardButton MapButton;
+
+        public List<StandardButton> Buttons;
+        Color color;
+
+        public override void Init()
+        {
+            switch (NogardGame.GameState)
+            {
+                case (NogardGame.GameStateEnum.MainMenu):
+                    MainMenuButtons();
+                    break;
+                case (NogardGame.GameStateEnum.GameActive):
+                    break;
+                case (NogardGame.GameStateEnum.Pause):
+                    break;
+                case (NogardGame.GameStateEnum.GameOver):
+                    break;
+                case (NogardGame.GameStateEnum.MapMaker):
+                    break;
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            switch (NogardGame.GameState)
+            {
+                case (NogardGame.GameStateEnum.MainMenu):
+                    foreach (StandardButton b in Buttons)
+                    {
+                        b.Update(Color.Yellow);
+                    }
+                    break;
+                case (NogardGame.GameStateEnum.GameActive):
+                    break;
+                case (NogardGame.GameStateEnum.Pause):
+                    break;
+                case (NogardGame.GameStateEnum.GameOver):
+                    break;
+                case (NogardGame.GameStateEnum.MapMaker):
+                    break;
+            }
+        }
+
+        public override void Draw()
+        {
+            switch (NogardGame.GameState)
+            {
+                case (NogardGame.GameStateEnum.MainMenu):
+                    color = Color.Goldenrod;
+                    PlayButton.DrawStandardButton(5, "Play", 1);
+                    ScoreButton.DrawStandardButton(5, "Highscore", 0.5f);
+                    ExitButton.DrawStandardButton(5, "Exit", 0.4f);
+                    MapButton.DrawStandardButton(5, "MapMaker", 0.4f);
+                    break;
+                case (NogardGame.GameStateEnum.GameActive):
+                    break;
+                case (NogardGame.GameStateEnum.Pause):
+                    break;
+                case (NogardGame.GameStateEnum.GameOver):
+                    break;
+                case (NogardGame.GameStateEnum.MapMaker):
+                    break;
+            }
+        }
+
+        public void MainMenuButtons()
+        {
+            Buttons = new List<StandardButton>();
+            PlayButton = new StandardButton(new Rectangle(600, 110, 250, 120));
+            Buttons.Add(PlayButton);
+            ScoreButton = new StandardButton(new Rectangle(600, 250, 250, 70));
+            Buttons.Add(ScoreButton);
+            ExitButton = new StandardButton(new Rectangle(680, 340, 90, 55));
+            Buttons.Add(ExitButton);
+            MapButton = new StandardButton(new Rectangle(10, 635, 230, 55));
+            Buttons.Add(MapButton);
+        }
+    }
+}
---
+++ NogardTheDragon/Managers/MainMenuManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using NogardTheDragon.Utilities;
+
+namespace NogardTheDragon.Managers
+{
+    public class MainMenuManager : BaseManager
+    {
+        private NogardGame game;
+        
+        public MainMenuManager(NogardGame game)
+        {
+            this.game = game;
+        }
+
+        public override void Init()
+        {
+            NogardGame.GameState = NogardGame.GameStateEnum.MainMenu;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            NogardGame.ButtonManager.Update(gameTime);
+
+            foreach (StandardButton b in NogardGame.ButtonManager.Buttons)
+            {
+                if (b.buttonClicked && b.Equals(NogardGame.ButtonManager.PlayButton))
+                {
+                    NogardGame.GamePlayManager.Init();
+                }
+                if (b.buttonClicked && b.Equals(NogardGame.ButtonManager.ScoreButton))
+                {
+
+                }
+                if (b.buttonClicked && b.Equals(NogardGame.ButtonManager.ExitButton))
+                {
+                    game.Exit();
+                }
+                if (b.buttonClicked && b.Equals(NogardGame.ButtonManager.MapButton))
+                {
+                    NogardGame.MapMakerManager.Init();
+                }
+            }
+        }
+
+        public override void Draw()
+        {
+            NogardGame.SpriteBatch.Draw(TextureManager.MainMenuBackTex, new Vector2(-100, 0), Color.White);
+            NogardGame.ButtonManager.Draw();
+        }
+    }
+}
