fbd4309f4e6b09aee62fc2076d7a3de624a4674e
MartinMidtgaard <martinmidtgaard@yahoo.com>
2017-04-27 15:36:40 +0200

    Walking for enemy type

--- NogardTheDragon/Objects/Enemies/StandardEnemy.cs
+++ NogardTheDragon/Objects/Enemies/StandardEnemy.cs
@@ -13,5 +13,27 @@ namespace NogardTheDragon.Objects
         public StandardEnemy(Vector2 pos, Texture2D tex) : base(pos, tex)
         {
         }
+            public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if (DrawPos.X <= DrawPos.X - 5)
+            {
+                Walk = false;
+            }
+            else if (DrawPos.X >= DrawPos.X + 5)
+            {
+                Walk = true;
+            }
+
+            if (Walk == false)
+            {
+                DrawPos.X += 1f;
+            }
+            else if (Walk == true)
+            {
+                DrawPos.X -= 1;
+            }
+        }
     }
 }
