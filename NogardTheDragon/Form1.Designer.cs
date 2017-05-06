c8767ce3918f367c781a43556bf65355702d5dd8
ObettyB <bettybrandstrom92@hotmail.se>
2017-05-06 16:33:09 +0200

    Satt en max längd namn
    maxlängden på ett namn som en spelare kan skriva in när de sparar sitt
    highscore är 16 tecken

--- NogardTheDragon/Form1.Designer.cs
+++ NogardTheDragon/Form1.Designer.cs
@@ -48,6 +48,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(67, 70);
+            this.textBox1.MaxLength = 16;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(174, 22);
             this.textBox1.TabIndex = 1;
